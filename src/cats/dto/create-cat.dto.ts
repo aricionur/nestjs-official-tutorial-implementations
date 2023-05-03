a74bcf7a287be93a6698aac17d627f29667cd16f
aricionur <aricionur@gmail.com>
2023-05-03 12:44:53 +0300

    Global validation pipe was added

--- src/cats/dto/create-cat.dto.ts
+++ src/cats/dto/create-cat.dto.ts
@@ -1,12 +1,26 @@
+import { IsString, IsInt } from 'class-validator';
+
 export class CreateCatDto {
+  @IsString()
   name: string;
+
+  @IsInt()
   age: number;
+
+  @IsString()
   breed: string;
 }
 
 export class UpdateCatDto {
+  @IsInt()
   id: number;
+
+  @IsString()
   name: string;
-  age?: number;
+
+  @IsInt()
+  age: number;
+
+  @IsString()
   breed?: string;
 }
--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,5 @@
 import { NestFactory, HttpAdapterHost } from '@nestjs/core';
+import { ValidationPipe } from '@nestjs/common';
 
 import { AppModule } from './app.module';
 import { logger } from './middlewares/logger.middleware';
@@ -7,12 +8,16 @@ import { AllExceptionsFilter } from './util/exceptions/all-exceptions.filter';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
-  // Global middleware goes in here if you need
+  // Global middleware(s) goes in here if you need them.
   app.use(logger);
   // ...
 
   const { httpAdapter } = app.get(HttpAdapterHost);
   app.useGlobalFilters(new AllExceptionsFilter(httpAdapter));
+
+  app.useGlobalPipes(
+    new ValidationPipe({ whitelist: true, forbidNonWhitelisted: true }),
+  );
 
   await app.listen(3000);
 }
